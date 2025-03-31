1281f9b035e951422827febadc1cc056687c1b0a
GIVEMEFOOD <danielallcorn5@gmail.com>
2025-03-31 14:51:47 +0100

    rawr

--- Start.cpp
+++ Start.cpp
@@ -1,12 +1,70 @@
 #include <iostream>
+#include <string>
 
 int x;
+int y;
+int sum;
+std::string calculation;
+
+void addition() 
+{
+   std::cout << "Type a number: ";
+   std::cin >> x;
+   std::cout << "Enter another number: ";
+   std::cin >> y;
+   sum = x + y;
+   std::cout << "the sum of your numbers are: " << sum;
+}
+
+void subtraction() 
+{
+   std::cout << "Type a first: ";
+   std::cin >> x;
+   std::cout << "Type a second number: ";
+   std::cin >> y;
+   sum = x - y;
+   std::cout << "the sum of your numbers are: " << sum;
+}
+
+void divide() 
+{
+   std::cout << "Type a number: ";
+   std::cin >> x;
+   std::cout << "Enter another number: ";
+   std::cin >> y;
+   sum = x / y;
+   std::cout << "the sum of your numbers are: " << sum;
+}
+
+void multiply()
+{
+   std::cout << "Type a number: ";
+   std::cin >> x;
+   std::cout << "Enter another number: ";
+   std::cin >> y;
+   sum = x * y;
+   std::cout << "the sum of your numbers are: " << sum;
+}
 
 int main()
 {
-   std::cout << 5;
-   std::cout << -6.7;
-   std::cout << 'H';
-   std::cout << "Hello world!\n";
+   std::cout << "enter the calculation you want: multiplication, addition, subtraction or division (please enter it like this else it wont work) ";
+   std::cin >> calculation;
+   if (calculation == "addition")
+   {
+      addition();
+   }
+   else if (calculation == "subtraction") 
+   {
+      subtraction();
+   }
+   else if (calculation == "multiplication")
+   {
+      multiply();
+   }
+   else
+   {
+      divide();
+   }
    return 0;
 }
